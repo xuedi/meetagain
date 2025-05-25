43bd2c69fea9d2a3442e31e7e56982ec97d1b903
Daniel Koch <xuedi@beijingcode.org>
2025-05-25 02:09:27 +0200

    live fixing

--- src/Controller/Admin/SystemController.php
+++ src/Controller/Admin/SystemController.php
@@ -26,7 +26,7 @@ class SystemController extends AbstractController
         $startTime = microtime(true);
         $cnt = 0;
         foreach ($userRepo->findAll() as $user) {
-            if ($user === null) {
+            if ($user->getImage() === null) {
                 continue;
             }
             $upload->createThumbnails($user->getImage(), [[50, 50], [400, 400]]);
