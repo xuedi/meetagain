7138c429ee3704782f8440fc9a86d0b98d378962
Daniel Koch <xuedi@beijingcode.org>
2025-06-02 20:55:30 +0200

    enhanced captcha service to generate and display image-based captchas

--- src/Service/CaptchaService.php
+++ src/Service/CaptchaService.php
@@ -2,13 +2,16 @@
 
 namespace App\Service;
 
-use Random\RandomException;
+use Imagick;
+use ImagickDraw;
+use ImagickPixel;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 readonly class CaptchaService
 {
     const int LENGTH = 4;
-    const string VALID_CHARACTERS = 'abcdefghijklmnpqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const string FONT = __DIR__ . '/../../assets/fonts/captcha.ttf';
+    const string VALID_CHARACTERS = 'abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ';
 
     private SessionInterface $session;
 
@@ -19,34 +22,58 @@ readonly class CaptchaService
 
     public function generate(): string
     {
-        $code = $this->session->get('captcha_' . $this->session->getId(), null);
-        if($code !== null) {
-            return $code;
+        $image = $this->session->get('captcha_image' . $this->session->getId(), null);
+        if($image !== null) {
+            return $image;
         }
         $code = '';
-        try {
-            foreach (range(0, self::LENGTH - 1) as $i) {
-                $code .= self::VALID_CHARACTERS[random_int(0, strlen(self::VALID_CHARACTERS) - 1)];
-            }
-        } catch (RandomException $e) {
-            $code = '0000';
+        foreach (range(0, self::LENGTH - 1) as $i) {
+            $code .= self::VALID_CHARACTERS[random_int(0, strlen(self::VALID_CHARACTERS) - 1)];
         }
-        $this->session->set('captcha_' . $this->session->getId(), $code);
+        $image = $this->generateImage($code);
 
-        return $code;
+        $this->session->set('captcha_text' . $this->session->getId(), $code);
+        $this->session->set('captcha_image' . $this->session->getId(), $image);
+
+        return $image;
     }
 
     public function isValidate(string $code): ?string
     {
-        $expected = $this->session->get('captcha_' . $this->session->getId());
+        $code = strtolower($code);
+        $expected = strtolower($this->session->get('captcha_text' . $this->session->getId(), null));
         if ($expected !== $code) {
-            return sprintf('Wrong captcha code, got %s but expected %s', $code, $expected);
+            return sprintf("Wrong captcha code, got '%s' but expected '%s'", $code, $expected);
         }
         return null;
     }
 
+    private function generateImage(string $code): string
+    {
+        $image = new Imagick();
+        $image->newImage(100, 60, new ImagickPixel("grey"));
+        $image->setImageFormat('png');
+
+        $x = 8;
+        $baseY = 38;
+        $size = 25;
+        foreach (str_split($code) as $char) {
+            $angle = random_int(-20, 20);
+            $y = $baseY + random_int(-6, 6);
+            $draw = new ImagickDraw();
+            $draw->setFont(self::FONT);
+            $draw->setFontSize($size + random_int(-4, 6));;
+            $draw->setFillColor(sprintf('#%s0%s0%s0', random_int(0, 9), random_int(0, 9), random_int(0, 9)));;
+            $image->annotateImage($draw, $x, $y, $angle, $char);
+            $x += 20 + random_int(-4, 4);
+        }
+
+        return base64_encode($image->getimageblob());
+    }
+
     public function reset(): void
     {
-        $this->session->remove('captcha_' . $this->session->getId());
+        $this->session->remove('captcha_text' . $this->session->getId());
+        $this->session->remove('captcha_image' . $this->session->getId());
     }
 }
