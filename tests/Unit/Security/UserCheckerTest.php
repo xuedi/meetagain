b7bbdaa6f80c9430c96659865a8a80570216b430
Daniel Koch <xuedi@beijingcode.org>
2025-06-10 23:54:34 +0200

    add comprehensive unit tests for `UserChecker` class

--- tests/Unit/Security/UserCheckerTest.php
+++ tests/Unit/Security/UserCheckerTest.php
@@ -2,13 +2,19 @@
 
 namespace App\Tests\Unit\Security;
 
+use App\Entity\User;
+use App\Entity\UserStatus;
 use App\Repository\MessageRepository;
 use App\Security\UserChecker;
 use App\Service\ActivityService;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserCheckerTest extends TestCase
 {
@@ -32,5 +38,95 @@ class UserCheckerTest extends TestCase
             $this->requestStack,
             $this->msgRepo
         );
+    }
+
+    public function testAbortCheckPreAuthWithNonUserObject(): void
+    {
+        $nonUserObject = $this->createMock(UserInterface::class);
+        $this->subject->checkPreAuth($nonUserObject);
+
+        $this->assertTrue(true);
+    }
+
+    public function testCheckPreAuthWithActiveUser(): void
+    {
+        $activeUser = $this->createMock(User::class);
+        $activeUser->method('getStatus')->willReturn(UserStatus::Active);
+
+        $this->subject->checkPreAuth($activeUser);
+
+        $this->assertTrue(true);
+    }
+
+    public function testCheckPreAuthWithNonActiveUser(): void
+    {
+        // Create a mock of User with non-Active status (e.g., Registered)
+        $nonActiveUser = $this->createMock(User::class);
+        $nonActiveUser->method('getStatus')->willReturn(UserStatus::Registered);
+
+        // This should throw a CustomUserMessageAccountStatusException
+        $this->expectException(CustomUserMessageAccountStatusException::class);
+        $this->expectExceptionMessage('The user is not anymore or not jet active');
+
+        $this->subject->checkPreAuth($nonActiveUser);
+    }
+
+    public function testWillAbortInFailureOfGettingUser(): void
+    {
+        $nonUserObject = $this->createMock(UserInterface::class);
+        $this->subject->checkPostAuth($nonUserObject);
+
+        $this->assertTrue(true);
+    }
+
+    public function testWillAbortInFailureOfGettingRequest(): void
+    {
+        $this->requestStack->method('getCurrentRequest')->willReturn(null);
+
+        $this->subject->checkPostAuth($this->createMock(User::class));
+
+        $this->assertTrue(true);
+    }
+
+    public function testSuccessfulOnPostAuth(): void
+    {
+        $sessionMock = $this->createMock(SessionInterface::class);
+        $sessionMock
+            ->expects($this->exactly(2))
+            ->method('set');
+
+        $requestMock = $this->createMock(Request::class);
+        $requestMock
+            ->method('getSession')
+            ->willReturn($sessionMock);
+
+        $this->requestStack
+            ->method('getCurrentRequest')
+            ->willReturn($requestMock);
+
+        $this->em
+            ->expects($this->once())
+            ->method('persist');
+
+        $this->em
+            ->expects($this->once())
+            ->method('flush');
+
+        $this->em
+            ->expects($this->once())
+            ->method('flush');
+
+        $this->activityService
+            ->expects($this->once())
+            ->method('log');
+
+        $this->msgRepo
+            ->expects($this->once())
+            ->method('hasNewMessages')
+            ->willReturn(true);
+
+        $this->subject->checkPostAuth($this->createMock(User::class));
+
+        $this->assertTrue(true);
     }
 }
