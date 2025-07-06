fc8b7e5f4c0d57d73af5d68df594c645e2dc7f2a
Daniel Koch <xuedi@beijingcode.org>
2025-07-06 20:58:06 +0200

    refactor: simplify API routes by removing redundant `/api` prefix

--- src/Controller/NonLocale/ApiController.php
+++ src/Controller/NonLocale/ApiController.php
@@ -8,19 +8,22 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/api/', name: 'app_api')]
+    #[Route('/', name: 'app_api')]
     public function index(): Response
     {
         return $this->render('_non_locale/api.html.twig');
     }
-    #[Route('/api/translations', name: 'app_api_translations', methods: ['GET'])]
+
+    #[Route('/translations', name: 'app_api_translations', methods: ['GET'])]
     public function translationsIndex(TranslationRepository $repo): Response
     {
         return new JsonResponse($repo->getExportList());
     }
-    #[Route('/api/glossary', name: 'app_api_glossary', methods: ['GET'])]
+
+    #[Route('/glossary', name: 'app_api_glossary', methods: ['GET'])]
     public function glossaryIndex(): Response
     {
         return new JsonResponse('glossary');
