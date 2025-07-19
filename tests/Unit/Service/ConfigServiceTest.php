9a17c84285ce5e4f515bcf13a3bbff6008b343ef
Daniel Koch <xuedi@beijingcode.org>
2025-07-19 18:20:53 +0200

    enhance ConfigServiceTest with data providers and additional test cases

--- tests/Unit/Service/ConfigServiceTest.php
+++ tests/Unit/Service/ConfigServiceTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Unit\Service;
 
+use App\Entity\Config;
 use App\Entity\ImageType;
 use App\Repository\ConfigRepository;
 use App\Service\ConfigService;
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -21,49 +24,118 @@ class ConfigServiceTest extends TestCase
         );
     }
 
-    public function testGetThumbnailSizesForProfilePicture(): void
+    #[DataProvider('getThumbnailSizeMatrix')]
+    public function testThumbnailSizesFetching(ImageType $imageType, array $expected): void
     {
-        $sizes = $this->subject->getThumbnailSizes(ImageType::ProfilePicture);
-        $expected = [
-            [400, 400],
-            [80, 80],
-            [50, 50]
-        ];
-
-        $this->assertSame($expected, $sizes);
+        $this->assertSame(
+            $expected,
+            $this->subject->getThumbnailSizes($imageType)
+        );
     }
 
-    public function testGetThumbnailSizesForEventTeaser(): void
+    public static function getThumbnailSizeMatrix(): Generator
     {
-        $sizes = $this->subject->getThumbnailSizes(ImageType::EventTeaser);
-        $expected = [
-            [1024, 768],
-            [600, 400],
-            [210, 140]
+        yield 'test profile picture' => [
+            ImageType::ProfilePicture, [
+                [400, 400],
+                [80, 80],
+                [50, 50],
+            ],
         ];
-
-        $this->assertSame($expected, $sizes);
+        yield 'test event teaser' => [
+            ImageType::EventTeaser, [
+                [1024, 768],
+                [600, 400],
+                [210, 140],
+            ],
+        ];
+        yield 'test event upload' => [
+            ImageType::EventUpload, [
+                [1024, 768],
+                [210, 140],
+            ],
+        ];
+        yield 'test cms block' => [
+            ImageType::CmsBlock, [
+                [432, 432],
+                [80, 80],
+            ],
+        ];
     }
 
-    public function testGetThumbnailSizesForEventUpload(): void
+    public function testThumbnailSizeList(): void
     {
-        $sizes = $this->subject->getThumbnailSizes(ImageType::EventUpload);
         $expected = [
-            [1024, 768],
-            [210, 140]
+            '1024x768' => 0,
+            '600x400' => 0,
+            '432x432' => 0,
+            '400x400' => 0,
+            '210x140' => 0,
+            '80x80' => 0,
+            '50x50' => 0,
         ];
-
-        $this->assertSame($expected, $sizes);
+        $this->assertSame(
+            $expected,
+            $this->subject->getThumbnailSizeList()
+        );
     }
 
-    public function testGetThumbnailSizesForCmsBlock(): void
+    #[DataProvider('getThumbnailSizeCheckerMatrix')]
+    public function testThumbnailSizeChecker(ImageType $imageType, int $width, int $height, bool $expected): void
     {
-        $sizes = $this->subject->getThumbnailSizes(ImageType::CmsBlock);
-        $expected = [
-            [432, 432],
-            [80, 80],
-        ];
+        $this->assertSame(
+            $expected,
+            $this->subject->isValidThumbnailSize($imageType, $width, $height)
+        );
+    }
 
-        $this->assertSame($expected, $sizes);
+    public static function getThumbnailSizeCheckerMatrix(): Generator
+    {
+        yield [ImageType::ProfilePicture, 1024, 768, false];
+        yield [ImageType::ProfilePicture, 600, 400, false];
+        yield [ImageType::ProfilePicture, 432, 432, false];
+        yield [ImageType::ProfilePicture, 400, 400, true];
+        yield [ImageType::ProfilePicture, 210, 140, false];
+        yield [ImageType::ProfilePicture, 80, 80, true];
+        yield [ImageType::ProfilePicture, 50, 50, true];
+        yield [ImageType::ProfilePicture, 128000, 51200, false];
+    }
+
+    public function testHostGetter(): void
+    {
+        $expected = 'http://localhost';
+        $this->assertSame(
+            $expected,
+            $this->subject->getHost()
+        );
+    }
+
+    public function testFrontPageToggleOn(): void
+    {
+        $this->configRepoMock
+            ->expects($this->once())
+            ->method('findOneBy')
+            ->with(['name' => 'show_frontpage'])
+            ->willReturn(new Config()->setValue('true'));
+
+        $this->assertTrue($this->subject->isShowFrontpage());
+    }
+
+    public function testFrontPageToggleOff(): void
+    {
+        $this->configRepoMock
+            ->method('findOneBy')
+            ->willReturn(new Config()->setValue('false'));
+
+        $this->assertFalse($this->subject->isShowFrontpage());
+    }
+
+    public function testFrontPageToggleWithDefault(): void
+    {
+        $this->configRepoMock
+            ->method('findOneBy')
+            ->willReturn(null);
+
+        $this->assertFalse($this->subject->isShowFrontpage());
     }
 }
