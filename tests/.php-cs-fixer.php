050392b0204b79e5de21f4edf0e20e8647674941
xuedi <xuedi.koch@getaway.group>
2025-12-26 14:59:11 +0100

    Update PHP-CS-Fixer config and local settings for improved checks and parallel processing

--- tests/.php-cs-fixer.php
+++ tests/.php-cs-fixer.php
@@ -6,15 +6,14 @@ $finder = (new PhpCsFixer\Finder())
         __DIR__ . '/../tests',
         __DIR__ . '/../public/install',
     ])
-    ->exclude('var')
-;
+    ->exclude('var');
 
 return (new PhpCsFixer\Config())
     ->registerCustomFixers(new PhpCsFixerCustomFixers\Fixers())
     ->setRules([
-        '@Symfony' => true,
+        //'@Symfony' => true,
         'PhpCsFixerCustomFixers/declare_after_opening_tag' => true,
     ])
     ->setFinder($finder)
     ->setCacheFile(__DIR__ . '/../var/cache/.php-cs-fixer.cache')
-;
+    ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig());
