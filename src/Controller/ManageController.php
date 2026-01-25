8f5940405ae148eb8f4942fe0bc0732737878654
Daniel Koch <xuedi@beijingcode.org>
2026-01-25 22:52:15 +0100

    "Move translation suggestions management from `Manage` to the `Admin` dashboard; update controllers, templates, and tests to reflect the changes"

--- src/Controller/ManageController.php
+++ src/Controller/ManageController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\TranslationSuggestionStatus;
-use App\Repository\TranslationSuggestionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,19 +9,11 @@ class ManageController extends AbstractController
 {
     public const string ROUTE_MANAGE = 'app_manage';
 
-    public function __construct(
-        private readonly TranslationSuggestionRepository $translationSuggestionRepo,
-    ) {
-    }
-
     #[Route('/manage', name: self::ROUTE_MANAGE)]
     public function index(): Response
     {
-        return $this->render('manage/index.html.twig', [
-            'translationSuggestions' => $this->translationSuggestionRepo->findBy(
-                ['status' => TranslationSuggestionStatus::Requested],
-                ['createdAt' => 'DESC'],
-            ),
-        ]);
+        // Placeholder for future functionality
+        // Translation suggestions moved to admin dashboard
+        return $this->redirectToRoute('app_default');
     }
 }
