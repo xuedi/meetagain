656b466fe201e85868bda2fcf6a5bf0032380e3e
Daniel Koch <xuedi@beijingcode.org>
2026-02-11 22:47:38 +0100

    Add `#[IsGranted('ROLE_ADMIN')]` to `UserController` for role-based access control enforcement

--- plugins/adminTables/src/Controller/UserController.php
+++ plugins/adminTables/src/Controller/UserController.php
@@ -13,7 +13,9 @@ use Plugin\AdminTables\Form\UserType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractAdminController
 {
     public function getAdminNavigation(): ?AdminNavigationConfig
