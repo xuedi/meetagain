4684fe7659af028b6883b77b0a489fceac28671f
Daniel Koch <xuedi@beijingcode.org>
2026-02-11 22:46:24 +0100

    Add `#[IsGranted('ROLE_ADMIN')]` to controllers for consistent role-based access control validation.

--- plugins/adminTables/src/Controller/HostController.php
+++ plugins/adminTables/src/Controller/HostController.php
@@ -11,7 +11,9 @@ use Plugin\AdminTables\Form\HostType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class HostController extends AbstractAdminController
 {
     public function getAdminNavigation(): ?AdminNavigationConfig
