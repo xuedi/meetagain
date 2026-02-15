1bdf77017dbb45921b92a726ddfdb326bb03d4d3
Daniel Koch <xuedi@beijingcode.org>
2026-02-15 17:40:16 +0100

    Split SystemController into modular controllers (Config, Images, Theme) and restructure corresponding templates for improved maintainability and navigation.

---
+++ src/Controller/Admin/System/ConfigController.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Admin\System;
+
+use App\Controller\Admin\AbstractAdminController;
+use App\Controller\Admin\AdminNavigationConfig;
+use App\Entity\AdminLink;
+use App\Form\SettingsType;
+use App\Service\ConfigService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN'), Route('/admin/system')]
+class ConfigController extends AbstractAdminController
+{
+    public function getAdminNavigation(): ?AdminNavigationConfig
+    {
+        return null;
+    }
+
+    public function __construct(
+        private readonly ConfigService $configService,
+    ) {}
+
+    #[Route('', name: 'app_admin_system')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_admin_system_config');
+    }
+
+    #[Route('/config', name: 'app_admin_system_config', methods: ['GET', 'POST'])]
+    public function config(Request $request): Response
+    {
+        $form = $this->createForm(SettingsType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->configService->saveForm($form->getData());
+            $this->addFlash('success', 'Settings saved');
+        }
+
+        return $this->render('admin/system/config/index.html.twig', [
+            'active' => 'system',
+            'form' => $form,
+            'config' => $this->configService->getBooleanConfigs(),
+        ]);
+    }
+
+    #[Route('/boolean/{name}', name: 'app_admin_system_boolean', methods: ['POST'])]
+    public function boolean(Request $request, string $name): Response
+    {
+        $value = $this->configService->toggleBoolean($name);
+
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse(['newStatus' => $value]);
+        }
+
+        return $this->redirectToRoute('app_admin_system_config');
+    }
+}
---
+++ src/Controller/Admin/System/ImagesController.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Admin\System;
+
+use App\Controller\Admin\AbstractAdminController;
+use App\Controller\Admin\AdminNavigationConfig;
+use App\Service\ImageService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN'), Route('/admin/system/images')]
+class ImagesController extends AbstractAdminController
+{
+    public function getAdminNavigation(): ?AdminNavigationConfig
+    {
+        return null;
+    }
+
+    public function __construct(
+        private readonly ImageService $imageService,
+    ) {}
+
+    #[Route('', name: 'app_admin_system_images', methods: ['GET'])]
+    public function images(): Response
+    {
+        return $this->render('admin/system/images/index.html.twig', [
+            'active' => 'system',
+        ]);
+    }
+
+    #[Route('/regenerate_thumbnails', name: 'app_admin_regenerate_thumbnails', methods: ['POST'])]
+    public function regenerateThumbnails(): Response
+    {
+        $startTime = microtime(true);
+        $cnt = $this->imageService->regenerateAllThumbnails();
+        $executionTime = microtime(true) - $startTime;
+
+        $this->addFlash('success', 'Regenerated thumbnails for ' . $cnt . ' images in ' . $executionTime . ' seconds');
+
+        return $this->redirectToRoute('app_admin_system_images');
+    }
+
+    #[Route('/cleanup_thumbnails', name: 'app_admin_cleanup_thumbnails', methods: ['POST'])]
+    public function cleanupThumbnails(): Response
+    {
+        $startTime = microtime(true);
+        $cnt = $this->imageService->deleteObsoleteThumbnails();
+        $executionTime = microtime(true) - $startTime;
+
+        $this->addFlash('success', 'Deleted ' . $cnt . ' obsolete thumbnail in ' . $executionTime . ' seconds');
+
+        return $this->redirectToRoute('app_admin_system_images');
+    }
+}
