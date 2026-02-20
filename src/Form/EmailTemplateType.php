ba03fc52cb4e17b8ac386b6bb77b10d2910d9f39
Daniel Koch <xuedi@beijingcode.org>
2026-02-20 19:03:17 +0100

    Switch to `getLanguageCodes` for broader language support in Email Template management.

--- src/Form/EmailTemplateType.php
+++ src/Form/EmailTemplateType.php
@@ -27,7 +27,7 @@ class EmailTemplateType extends AbstractType
         $templateId = $template?->getId();
 
         if ($templateId !== null) {
-            foreach ($this->translationService->getAdminLanguageCodes() as $languageCode) {
+            foreach ($this->translationService->getLanguageCodes() as $languageCode) {
                 $translation = $this->translationRepo->findOneBy([
                     'emailTemplate' => $templateId,
                     'language' => $languageCode,
