89458c413763ced2a403f8134affdb8ece26223d
Daniel Koch <xuedi@beijingcode.org>
2026-03-08 19:27:10 +0100

    Add `imageRight` support to Paragraph block entity, rendering logic, and editing interface.

--- src/Entity/BlockType/Paragraph.php
+++ src/Entity/BlockType/Paragraph.php
@@ -11,13 +11,14 @@ class Paragraph implements BlockType
     private function __construct(
         public string $title,
         public string $content,
+        public bool $imageRight,
         public ?ImageEntity $image,
     ) {}
 
     #[Override]
     public static function fromJson(array $json, ?ImageEntity $image = null): self
     {
-        return new self($json['title'], $json['content'], $image);
+        return new self($json['title'], $json['content'], (bool) ($json['imageRight'] ?? false), $image);
     }
 
     #[Override]
@@ -32,6 +33,7 @@ class Paragraph implements BlockType
         return [
             'title' => $this->title,
             'content' => $this->content,
+            'imageRight' => $this->imageRight,
             'image' => $this->image,
         ];
     }
