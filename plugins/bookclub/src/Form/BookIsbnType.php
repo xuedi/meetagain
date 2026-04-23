7c7e2dfec5b9b7815e7e30c4c064993c02aaf371
Daniel Koch <xuedi@beijingcode.org>
2026-04-24 01:05:45 +0200

    Improve Filmclub and Bookclub plugins' internationalization consistency, update test assertion logic, and refine admin navigation section parameter handling.

--- plugins/bookclub/src/Form/BookIsbnType.php
+++ plugins/bookclub/src/Form/BookIsbnType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -29,7 +28,7 @@ class BookIsbnType extends AbstractType
                 new NotBlank(),
                 new Regex(
                     pattern: '/^[\d\-\s X]+$/i',
-                    message: new TranslatableMessage('bookclub_book.error_isbn_invalid'),
+                    message: 'bookclub_book.error_isbn_invalid',
                 ),
             ],
         ]);
