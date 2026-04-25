c55133de6ad3c112bb94c1f30036feffef787b95
Daniel Koch <xuedi@beijingcode.org>
2026-04-25 22:23:34 +0200

    fixed translation

--- plugins/dinnerclub/src/Kernel.php
+++ plugins/dinnerclub/src/Kernel.php
@@ -31,7 +31,7 @@ class Kernel implements Plugin
     public function getLinkCollection(): LinkCollection
     {
         return LinkCollection::empty()->withNavLinks([
-            new Link(slug: $this->urlGenerator->generate('app_plugin_dinnerclub'), name: 'menu_dishes'),
+            new Link(slug: $this->urlGenerator->generate('app_plugin_dinnerclub'), name: 'dinnerclub.menu_dishes'),
         ]);
     }
 
