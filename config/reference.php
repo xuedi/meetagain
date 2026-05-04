f6c52a5293a8b01933075d656cb0632dc35bbdf9
Daniel Koch <xuedi@beijingcode.org>
2026-05-04 23:20:23 +0200

    Add support for NelmioApiDocBundle, RadebatzTypeInfoExtras, and update dependencies including Symfony Console and Swagger-PHP.

--- config/reference.php
+++ config/reference.php
@@ -1599,6 +1599,56 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     },
  *     role_prefix?: scalar|Param|null, // Set a custom prefix that replaces the default 'ROLE_OAUTH2_' role prefix // Default: "ROLE_OAUTH2_"
  * }
+ * @psalm-type NelmioApiDocConfig = array{
+ *     type_info?: bool|Param, // Use the symfony/type-info component for determining types. // Default: true
+ *     use_validation_groups?: bool|Param, // If true, `groups` passed to #[Model] attributes will be used to limit validation constraints // Default: false
+ *     operation_id_generation?: \Nelmio\ApiDocBundle\Describer\OperationIdGeneration::ALWAYS_PREPEND|\Nelmio\ApiDocBundle\Describer\OperationIdGeneration::CONDITIONALLY_PREPEND|\Nelmio\ApiDocBundle\Describer\OperationIdGeneration::NO_PREPEND|"always_prepend"|"conditionally_prepend"|"no_prepend"|Param, // How to generate operation ids // Default: "always_prepend"
+ *     cache?: array{
+ *         pool?: scalar|Param|null, // define cache pool to use // Default: null
+ *         item_id?: scalar|Param|null, // define cache item id // Default: null
+ *     },
+ *     documentation?: array<string, mixed>,
+ *     media_types?: list<scalar|Param|null>,
+ *     html_config?: array{ // UI configuration options
+ *         assets_mode?: scalar|Param|null, // Default: "cdn"
+ *         swagger_ui_config?: array<mixed>,
+ *         redocly_config?: array<mixed>,
+ *         scalar_config?: array<mixed>,
+ *         stoplight_config?: array<mixed>,
+ *     },
+ *     areas?: array<string, array{ // Default: {"default":{"path_patterns":[],"host_patterns":[],"with_attribute":false,"documentation":[],"name_patterns":[],"disable_default_routes":false,"cache":[],"security":[]}}
+ *             path_patterns?: list<scalar|Param|null>,
+ *             host_patterns?: list<scalar|Param|null>,
+ *             name_patterns?: list<scalar|Param|null>,
+ *             security?: array<string, array{ // Default: []
+ *                     type?: scalar|Param|null,
+ *                     scheme?: scalar|Param|null,
+ *                     in?: scalar|Param|null,
+ *                     name?: scalar|Param|null,
+ *                     description?: scalar|Param|null,
+ *                     openIdConnectUrl?: scalar|Param|null,
+ *                     ...<string, mixed>
+ *                 }>,
+ *             with_attribute?: bool|Param, // whether to filter by attributes // Default: false
+ *             disable_default_routes?: bool|Param, // if set disables default routes without attributes // Default: false
+ *             documentation?: array<string, mixed>,
+ *             cache?: array{
+ *                 pool?: scalar|Param|null, // define cache pool to use // Default: null
+ *                 item_id?: scalar|Param|null, // define cache item id // Default: null
+ *             },
+ *         }>,
+ *     models?: array{
+ *         use_jms?: bool|Param, // Default: false
+ *         names?: list<array{ // Default: []
+ *                 alias?: scalar|Param|null,
+ *                 type?: scalar|Param|null,
+ *                 groups?: mixed, // Default: null
+ *                 options?: mixed, // Default: null
+ *                 serializationContext?: list<mixed>,
+ *                 areas?: list<scalar|Param|null>,
+ *             }>,
+ *     },
+ * }
  * @psalm-type NelmioCorsConfig = array{
  *     defaults?: array{
  *         allow_credentials?: bool|Param, // Default: false
@@ -1642,6 +1692,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     sentry?: SentryConfig,
  *     symfonycasts_sass?: SymfonycastsSassConfig,
  *     league_oauth2_server?: LeagueOauth2ServerConfig,
+ *     nelmio_api_doc?: NelmioApiDocConfig,
  *     nelmio_cors?: NelmioCorsConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
