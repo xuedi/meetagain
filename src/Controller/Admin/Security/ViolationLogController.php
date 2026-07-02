d341d03d5b88c10c7d435bc79b41a017cf67fed8
Daniel Koch <xuedi@beijingcode.org>
2026-07-02 18:28:23 +0200

    Add violation log functionality to admin security section

---
+++ src/Controller/Admin/Security/ViolationLogController.php
@@ -0,0 +1,104 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Admin\Security;
+
+use App\Admin\Navigation\AdminNavigationInterface;
+use App\Admin\Tabs\AdminTabsInterface;
+use App\Admin\Top\Actions\AdminTopActionButton;
+use App\Admin\Top\Actions\AdminTopActionDropdown;
+use App\Admin\Top\Actions\AdminTopActionDropdownOption;
+use App\Admin\Top\AdminTop;
+use App\Admin\Top\Infos\AdminTopInfoHtml;
+use App\Repository\AccessDeniedLogRepository;
+use App\Security\Permission\Attribute\PermissionAttribute;
+use DateTimeImmutable;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[IsGranted('ROLE_ADMIN')]
+final class ViolationLogController extends AbstractSecurityController implements AdminNavigationInterface, AdminTabsInterface
+{
+    private const string DEFAULT_RANGE = '24h';
+
+    /** @var array<string, string|null> */
+    private const array RANGE_OFFSETS = [
+        '24h' => '-24 hours',
+        '1w' => '-1 week',
+        '1m' => '-1 month',
+        'all' => null,
+    ];
+
+    public function __construct(
+        TranslatorInterface $translator,
+        private readonly AccessDeniedLogRepository $accessDeniedLogRepo,
+    ) {
+        parent::__construct($translator, 'permissions');
+    }
+
+    #[Route('/admin/security/permissions/violations', name: 'app_admin_security_permissions_violations', methods: ['GET'])]
+    public function list(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted(PermissionAttribute::SYSTEM_SECURITY_ACCESS_DENIED_READ);
+
+        $range = $request->query->getString('range', self::DEFAULT_RANGE);
+        if (!array_key_exists($range, self::RANGE_OFFSETS)) {
+            $range = self::DEFAULT_RANGE;
+        }
+        $rangeOffset = self::RANGE_OFFSETS[$range];
+        $since = $rangeOffset !== null ? new DateTimeImmutable($rangeOffset) : null;
+
+        $recent = $this->accessDeniedLogRepo->getRecent(200, $since);
+        $totalCount = $this->accessDeniedLogRepo->countAll();
+        $rangeCount = $since !== null ? $this->accessDeniedLogRepo->countSince($since) : $totalCount;
+
+        $info = [
+            new AdminTopInfoHtml(sprintf('<strong>%d</strong>&nbsp;%s', $totalCount, $this->translator->trans('admin_security.summary_total_access_denied'))),
+        ];
+        if ($rangeCount === 0) {
+            $info[] = new AdminTopInfoHtml(sprintf(
+                '<span class="tag is-success is-medium">%s</span>',
+                $this->translator->trans('admin_security.summary_no_access_denied_in_range'),
+            ));
+        } else {
+            $info[] = new AdminTopInfoHtml(sprintf('<strong>%d</strong>&nbsp;%s', $rangeCount, $this->translator->trans('admin_security.summary_in_range')));
+        }
+
+        $adminTop = new AdminTop(info: $info, actions: [
+            $this->buildRangeDropdown($range),
+            new AdminTopActionButton(
+                label: $this->translator->trans('global.button_back'),
+                target: $this->generateUrl('app_admin_security_permissions'),
+                icon: 'arrow-left',
+            ),
+        ]);
+
+        return $this->render('admin/security/violation_log.html.twig', [
+            'active' => 'security',
+            'rows' => $recent,
+            'adminTop' => $adminTop,
+            'adminTabs' => $this->getTabs(),
+        ]);
+    }
+
+    private function buildRangeDropdown(string $current): AdminTopActionDropdown
+    {
+        $options = [];
+        foreach (array_keys(self::RANGE_OFFSETS) as $key) {
+            $params = $key === self::DEFAULT_RANGE ? [] : ['range' => $key];
+            $options[] = new AdminTopActionDropdownOption(
+                label: $this->translator->trans('admin_logs.range_' . $key),
+                target: $this->generateUrl('app_admin_security_permissions_violations', $params),
+                isActive: $key === $current,
+            );
+        }
+
+        return new AdminTopActionDropdown(
+            label: sprintf('%s %s', $this->translator->trans('admin_logs.range_label'), $this->translator->trans('admin_logs.range_' . $current)),
+            options: $options,
+            icon: 'clock',
+        );
+    }
+}
