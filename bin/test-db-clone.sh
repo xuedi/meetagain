9cfd2b3be1d801d16622fb7a3f32d48c9214919a
xuedi <xuedi.koch@getaway.group>
2026-08-27 17:52:05 +0200

    fixed script & test

--- bin/test-db-clone.sh
+++ bin/test-db-clone.sh
@@ -22,13 +22,13 @@ template="${MARIADB_DATABASE}_test"
 dump_path="/tmp/${template}-template.sql"
 
 printf 'Dumping template DB %s ...\n' "$template"
-$DOCKER exec mariadb sh -c "mariadb-dump --user='${MARIADB_USER}' --password='${MARIADB_PASSWORD}' --no-tablespaces --single-transaction --quick '${template}' > '${dump_path}'"
+$DOCKER exec -T mariadb sh -c "mariadb-dump --user='${MARIADB_USER}' --password='${MARIADB_PASSWORD}' --no-tablespaces --single-transaction --quick '${template}' > '${dump_path}'"
 
 printf 'Cloning into %d per-process DBs ...\n' "$procs"
 i=1
 while [ "$i" -le "$procs" ]; do
     clone="${template}${i}"
-    $DOCKER exec mariadb sh -c "
+    $DOCKER exec -T mariadb sh -c "
         mariadb --user='${MARIADB_USER}' --password='${MARIADB_PASSWORD}' -e \"DROP DATABASE IF EXISTS \\\`${clone}\\\`; CREATE DATABASE \\\`${clone}\\\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\" &&
         mariadb --user='${MARIADB_USER}' --password='${MARIADB_PASSWORD}' '${clone}' < '${dump_path}'
     " &
@@ -36,6 +36,6 @@ while [ "$i" -le "$procs" ]; do
 done
 wait
 
-$DOCKER exec mariadb sh -c "rm -f '${dump_path}'"
+$DOCKER exec -T mariadb sh -c "rm -f '${dump_path}'"
 
 printf 'Per-process test DBs ready (%s1..%s%d)\n' "$template" "$template" "$procs"
